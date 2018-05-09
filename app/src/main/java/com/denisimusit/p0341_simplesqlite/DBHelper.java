d2b8fe0ab9814e9676301b401137ae98ea2f8938
den <d>
2018-05-09 16:36:23 +0300

    Make to method onCreate a SQL request

--- app/src/main/java/com/denisimusit/p0341_simplesqlite/DBHelper.java
+++ app/src/main/java/com/denisimusit/p0341_simplesqlite/DBHelper.java
@@ -3,6 +3,7 @@ package com.denisimusit.p0341_simplesqlite;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 public class DBHelper extends SQLiteOpenHelper {
 
@@ -16,6 +17,8 @@ public class DBHelper extends SQLiteOpenHelper {
     public static final String KEY_SURNAME = "surname";
     public static final String KEY_MAIL = "mail";
 
+    private static final String LOG_TAG = "My log";
+
     public DBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
 
@@ -23,6 +26,7 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
+        Log.d(LOG_TAG, "--- onCreate database ---");
 
         //создает таблицу  TABLE_CONTACTS с полями  KEY_ID, KEY_NAME,  KEY_MAIL, KEY_SURNAME
         db.execSQL("create table " + TABLE_CONTACTS + "(" + KEY_ID
@@ -35,6 +39,12 @@ public class DBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        Log.d(LOG_TAG, "--- dell table ---");
+        // удатет таблицу если она создана
+        db.execSQL("drop table if exists " + TABLE_CONTACTS);
 
+        //создает таблицу заново
+        Log.d(LOG_TAG, "--- onCreate table ---");
+        onCreate(db);
     }
 }
